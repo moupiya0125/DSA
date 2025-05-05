5496f26065f3066806c4a45f33f847a20349b4b1
moupiya0125 <moupiya2002garai@gmail.com>
2025-05-05 11:26:41 +0530

    verion 2

--- second.cpp
+++ second.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {    
-    int n1,n2;
+    int n1,n3;
 
     cout << "Enter two integer: ";
     cin >> n1;
-    cin >> n2;
+    cin >> n3;
 
     cout << "You entered " << n1;
-    cout << "You entered " << n2;        
+    cout << "You entered " << n3;        
     return 0;
 }
