0f8937f6d99619f8cd48eacd4e1b8b55e4f8af92
moupiya0125 <moupiya2002garai@gmail.com>
2025-05-06 11:04:56 +0530

    version 5

---
+++ third.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int n1,n3;
+
+    cout << "Enter two integer: ";
+    cin >> n1;
+    cin >> n3;
+
+    cout << "You entered " << n1;
+    cout << "You entered " << n3;        
+    return 0;
+}
