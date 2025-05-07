e154f208570b2068264c216e8c1692e8bb7060e3
moupiya0125 <moupiya2002garai@gmail.com>
2025-05-07 09:11:00 +0530

    Added pattern6.cpp

---
+++ Patterns/pattern6.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int  n,i,j;
+    cin>>n;
+    i=1;
+    while(i<=n){
+        j=1;
+        while (j<=n)
+        {
+            cout<<n-j+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+
+}
